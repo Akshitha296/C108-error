6d800a93203dcfae88679107b8d2ff2659b1856a
Akshitha296 <70026624+Akshitha296@users.noreply.github.com>
2021-07-13 00:06:40 -0400

    Update phone.py

--- phone.py
+++ phone.py
@@ -3,7 +3,7 @@ import plotly.figure_factory as ff
 import csv 
 import pandas as pd
 
-df = pd.read_csv("data.csv")
+df = pd.read_csv("phone.csv")
 
 fig = ff.create_distplot([df["Avg Rating"].tolist()], ["Avg Rating"], show_hist = False)
 fig.show()
